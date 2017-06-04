c4cefea1640ff15dec8d390754af951fa6a2f86d
emilypo89 <emilypo89@gmail.com>
2017-06-04 15:38:52 -0400

    giphy api url

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,34 +3,47 @@
       var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&limit=10&api_key=dc6zaTOxFJmzC";
       $(".button").click();
 
+      // displayMovieInfo function re-renders the HTML to display the appropriate content
+      function displayAnimal() {
+
+        var movie = $(this).attr("data-name");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + movie + "&limit=10&api_key=dc6zaTOxFJmzC";
+
+        // Creates AJAX call for the specific movie button being clicked
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response) {
+          console.log(response);
+          $("#animals-view").html("<p>Rated: " + data[i].rating + "</p>");
+          $("#animals-view").append("<img scr='" + data[i].embeded-url + "' alt='" + animal[i] + "'poster'>");
+
+        });
+        }
+
       // Function for displaying movie data
       function renderButtons() {
         $("#animals-view").html(" ");
-        // Loop through the array of movies, then generate buttons for each movie in the array
+        // Loops the array of animals and creates buttons for those animals
         for (var i = 0; i < animals.length; i++) {
-              var button = $("<button>");
-              button.attr("class", "button");
-              $("#animals-view").append(button);
-              button.html(animals[i]);
+           var button = $("<button>");
+          button.addClass("button");
+          button.attr("data-name", animals[i]);
+          button.text(animals[i]);
+          $("#animals-view").append(button);
             }
       }
 
-      // This function handles events where the add movie button is clicked
+      // add gif button is clicked
       $("#add-gif").on("click", function(event) {
-        // event.preventDefault() prevents submit button from trying to send a form.
-        // Using a submit button instead of a regular button allows the user to hit
-        // "Enter" instead of clicking the button if desired
         event.preventDefault();
-
-        // Write code to grab the text the user types into the input field
         var gifInput = $("#gif-input").val();
-        // Write code to add the new movie into the movies array
         animals.push(gifInput);
 
         $("#gif-input").val("");
-        // The renderButtons function is called, rendering the list of movie buttons
         renderButtons();
       });
 
-      // Calling the renderButtons function to display the initial list of movies
+      $(document).on("click", ".button", displayAnimal);
+
       renderButtons();
